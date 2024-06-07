9549bd922ab7d04761379561f5bdeeeddd29a982
Saravanan M <sarvan.m20@gmail.com>
2024-06-07 19:42:55 +0530

    create intent for payment done

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const express = require("express");
 const app = express();
 const Stripe = require("stripe");
 const bodyParser = require("body-parser");
+require("dotenv").config();
 
-const stripe = Stripe(process.env.STRIP_KEY);
+const stripe = Stripe(process.env.STRIPE_KEY);
 
 app.use(bodyParser.json());
 
@@ -15,6 +16,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // routes
+// 1. Create intent for payment
+app.post("/api/v1/create_intent", async (req, res) => {
+  try {
+    const paymentIntent = await stripe.paymentIntents.create(req.body);
+    res.json(paymentIntent);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 
 app.get("/", (req, res) => {
   return res.status(200).json({ status: true, message: "App is running" });
